388b39fe07c767cda47171b5b630af6fab114c8a
Felipe <felipegp996@gmail.com>
2025-07-17 22:25:22 -0300

    :wrench: Redirects user to game-view on app load

--- balatro-clone/src/app/page.tsx
+++ balatro-clone/src/app/page.tsx
@@ -1,6 +1,14 @@
+'use client'
+
 import Page from '@/app/game-view/(overview)/page';
+import { redirect } from 'next/navigation';
+import {useEffect} from 'react'
 
 export default function Home() {
+  useEffect(() => {
+    redirect('/game-view')
+  })
+
   return (
     <div className="items-center justify-items-center min-h-screen">
       <Page />
